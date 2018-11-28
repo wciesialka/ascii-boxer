f785e9e84a6630d09640421b34b5279f2059f1cb
William Ciesialka <wciesialka@gmail.com>
2018-11-27 23:51:29 -0500

    Added maximum line width option

--- ascii-boxer.py
+++ ascii-boxer.py
@@ -4,7 +4,7 @@ from math import floor
 import re
 
 R,G,B = 0,1,2
-MAX_WH = 128
+MAX_WH = 70 # a little less than the typical max cpl
 
 def get_file_path():
     path = input("Image to translate: ")
@@ -24,12 +24,11 @@ def get_image():
     else:
         return img, filepath
 
-def resize_if_needed(img):
+def resize_if_needed(img,desired_width=MAX_WH):
     size = img.size
     w, h = size[0], size[1]
-    compare = max(w,h)
-    if (compare > MAX_WH):
-        ratio = MAX_WH/compare
+    if (w > desired_width):
+        ratio = desired_width/w
         new_w, new_h = floor(w*ratio), floor(h*ratio)
         return img.resize((new_w,new_h), Image.ANTIALIAS)
     else:
@@ -54,13 +53,26 @@ def val_to_char(val,invert=False):
 
 def get_yn():
     i = input("[Y/N] ").lower()
-    if i == 'y' or i=='yes':
+    if i == 'y' or i=='yes' or i == 't' or i == 'true' or i == 'ye':
         return True
-    elif i =='n' or i=='no':
+    elif i =='n' or i=='no' or i == 'f' or i == 'false':
         return False
     else:
         print("Please enter Y for 'Yes' or N for 'No'")
         return get_invert()
+
+def get_line_width():
+    try:
+        i = int(input("Maximum line width: "))
+    except:
+        print("Must be an integer.")
+        return get_line_width()
+    else:
+        if i <= 0:
+            print("Must be greater than zero.")
+            return get_line_width()
+        else:
+            return i
 
 def write_to_file(text,fallback_filename="ascii-art"):
     fn = input("File path: ")
@@ -111,7 +123,8 @@ def main():
     img, fbfn = get_image()
     fbfn = os.path.basename(fbfn)
     fbfn = fbfn[:fbfn.rfind(".")]
-    img = resize_if_needed(img).convert(mode="HSV")
+    line_width = get_line_width()
+    img = resize_if_needed(img,line_width).convert(mode="HSV")
     size = img.size
     w,h = size
     print("Invert colors? (Helps if text displayed is a light color on a dark background.)")
